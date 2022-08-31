904b9ad61bcfedde9406fcdb41ac2292a0dbf831
Jibhey <47273038+jbbergy@users.noreply.github.com>
2022-09-01 01:20:01 +0200

    Manage volume with slider and change progress to range for track progress

--- src/components/m-player/m-player.vue
+++ src/components/m-player/m-player.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="m-player">
+    <input
+      type="range"
+      min="0"
+      max="1"
+      step="0.1"
+      :value="currentVolume"
+      @input="setVolume"
+    >
     <button @click="stop">stop</button>
     <button @click="pause">pause</button>
     <button @click="play">play</button>
-    {{ progressBarValue }}%
+    <input
+      type="range"
+      min="0"
+      max="100"
+      :value="progressBarValue"
+    >
     <audio id="audioPlayer" />
   </div>
 </template>
@@ -17,9 +30,10 @@ const howlerObj = ref();
 let isPaused = ref(false);
 let seek = ref(0);
 let duration = ref(0);
+let currentVolume = ref(0.3);
+let intervalId: NodeJS.Timeout | null = null;
 
 const store = useGlobalStore();
-let intervalId: NodeJS.Timeout | null = null;
 const progressBarValue = computed(() => {
   let result = 0;
   if (duration.value > 0) {
@@ -33,19 +47,23 @@ watch(fileToPlay, () => {
   stop();
   play();
 });
+watch(currentVolume, (value) => {
+  Howler.volume(value);
+});
 
+function setVolume(event) {
+  currentVolume.value = event.target.value;
+}
 function stop() {
   if (howlerObj.value?.playing() || isPaused.value === true) {
     howlerObj.value.stop();
     isPaused.value = false;
   }
 }
-
 function pause() {
   howlerObj.value.pause();
   isPaused.value = true;
 }
-
 function play() {
   if (isPaused.value === false) {
     if (!!intervalId) {
@@ -56,7 +74,6 @@ function play() {
       src: [fileToPlay.value],
       html5: Howler.usingWebAudio,
     });
-    howlerObj.value.volume(0.3);
     howlerObj.value.on('load', function (e) {
       duration.value = howlerObj.value.duration();
     });
